66b7b731163fd66edff4bca67eacdc3afb3cb617
Dustin Sallings <dustin@spy.net>
2009-04-07 15:44:57 -0700

    Process ad-hoc commands.

--- c/memagent.c
+++ c/memagent.c
@@ -53,6 +53,99 @@ int version_handler(xmpp_conn_t * const conn, xmpp_stanza_t * const stanza,
 	return 1;
 }
 
+xmpp_stanza_t* error_unknown_command(const char *cmd, xmpp_conn_t* const conn,
+                                     xmpp_stanza_t * const stanza,
+                                     void * const userdata)
+{
+	xmpp_stanza_t *reply, *error, *condition;
+    agent_handle_t *handle = (agent_handle_t*) userdata;
+	xmpp_ctx_t *ctx = handle->ctx;
+
+    fprintf(stderr, "Unknown command:  %s\n", cmd);
+
+	reply = xmpp_stanza_new(ctx);
+    assert(reply);
+	xmpp_stanza_set_name(reply, "iq");
+	xmpp_stanza_set_type(reply, "error");
+	xmpp_stanza_set_id(reply, xmpp_stanza_get_id(stanza));
+	xmpp_stanza_set_attribute(reply, "to", xmpp_stanza_get_attribute(stanza, "from"));
+
+    /* Error element */
+	error = xmpp_stanza_new(ctx);
+    assert(error);
+	xmpp_stanza_set_name(error, "error");
+	xmpp_stanza_add_child(reply, error);
+    xmpp_stanza_set_attribute(error, "code", "404");
+    xmpp_stanza_set_attribute(error, "type", "modify");
+
+    /* Error condition */
+    condition = xmpp_stanza_new(ctx);
+    xmpp_stanza_set_name(condition, "item-not-found");
+    xmpp_stanza_set_ns(condition, "urn:ietf:params:xml:ns:xmpp-stanzas");
+    xmpp_stanza_add_child(error, condition);
+
+    return reply;
+}
+
+xmpp_stanza_t* process_serverlist(const char *cmd,
+                                  const xmpp_stanza_t* cmd_stanza,
+                                  xmpp_conn_t * const conn,
+                                  xmpp_stanza_t * const stanza,
+                                  void * const userdata)
+{
+    xmpp_stanza_t *reply, *req;
+	char *buf;
+	size_t len;
+    agent_handle_t *handle = (agent_handle_t*) userdata;
+	xmpp_ctx_t *ctx = handle->ctx;
+
+    fprintf(stderr, "Processing a serverlist.\n");
+
+	reply = xmpp_stanza_new(ctx);
+	xmpp_stanza_set_name(reply, "iq");
+	xmpp_stanza_set_type(reply, "result");
+	xmpp_stanza_set_id(reply, xmpp_stanza_get_id(stanza));
+	xmpp_stanza_set_attribute(reply, "to",
+                              xmpp_stanza_get_attribute(stanza, "from"));
+
+    return reply;
+}
+
+int command_handler(xmpp_conn_t * const conn, xmpp_stanza_t * const stanza,
+                    void * const userdata)
+{
+	xmpp_stanza_t *reply, *req;
+	char *buf;
+	size_t len;
+    agent_handle_t *handle = (agent_handle_t*) userdata;
+	xmpp_ctx_t *ctx = handle->ctx;
+    char *cmd;
+
+    fprintf(stderr, "Received a command from %s\n",
+            xmpp_stanza_get_attribute(stanza, "from"));
+
+    /* Figure out what the command is */
+    req = xmpp_stanza_get_children(stanza);
+    assert(req);
+    cmd = xmpp_stanza_get_attribute(req, "node");
+    assert(cmd);
+
+    fprintf(stderr, "Command:  %s\n", cmd);
+
+    if (strcmp(cmd, "serverlist") == 0) {
+        reply = process_serverlist(cmd, req, conn, stanza, handle);
+    } else {
+        reply = error_unknown_command(cmd, conn, stanza, handle);
+    }
+
+    if (reply) {
+        xmpp_send(conn, reply);
+        xmpp_stanza_release(reply);
+    }
+
+    return 1;
+}
+
 void conn_handler(xmpp_conn_t * const conn, const xmpp_conn_event_t status,
                   const int error, xmpp_stream_error_t * const stream_error,
                   void * const userdata)
@@ -63,6 +156,8 @@ void conn_handler(xmpp_conn_t * const conn, const xmpp_conn_event_t status,
         xmpp_stanza_t* pres;
         fprintf(stderr, "DEBUG: connected\n");
         xmpp_handler_add(conn, version_handler, "jabber:iq:version", "iq", NULL, handle);
+        xmpp_handler_add(conn, command_handler, "http://jabber.org/protocol/commands",
+                         "iq", NULL, handle);
         /*
         xmpp_handler_add(conn, message_handler, NULL, "message", NULL, ctx);
         */
