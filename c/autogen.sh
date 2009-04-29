0849a575a3b8589a05c259eadcaeeaa9f3c117d5
Dustin Sallings <dustin@spy.net>
2009-04-28 21:58:25 -0700

    Switched to automake.

---
+++ c/autogen.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# This is hacky, because there are so many damn versions
+# of autoconf/automake.  It works with Debian woody, at least.
+#
+# Debian sarge:
+#    apt-get install automake1.7 autoconf
+#
+
+echo "aclocal..."
+ACLOCAL=`which aclocal-1.10 || which aclocal-1.9 || which aclocal19 || which aclocal-1.7 || which aclocal17 || which aclocal-1.5 || which aclocal15 || which aclocal || exit 1`
+$ACLOCAL || exit 1
+
+echo "autoheader..."
+AUTOHEADER=${AUTOHEADER:-autoheader}
+$AUTOHEADER || exit 1
+
+echo "automake..."
+AUTOMAKE=`which automake-1.10 || which automake-1.9 || which automake-1.7 || exit 1`
+$AUTOMAKE --foreign --add-missing || automake --gnu --add-missing || exit 1
+
+echo "autoconf..."
+AUTOCONF=${AUTOCONF:-autoconf}
+$AUTOCONF || exit 1
+
