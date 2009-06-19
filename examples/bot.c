33409af88d2148acc272183c5cb6b65ef3ba5d2d
Dustin Sallings <dustin@spy.net>
2009-06-18 23:36:24 -0700

    Comment the bot a bit more since it's used as reference now.

--- examples/bot.c
+++ examples/bot.c
@@ -6,6 +6,16 @@
 
 #include "conflate.h"
 
+/*
+ * This program is both an example of most of the public API of
+ * libconflate as well as a sample program to interact with it.
+ */
+
+/* ************************************************************ */
+
+/*
+ * Example callback to handle a newly receive configuration.
+ */
 static void display_config(void* userdata, kvpair_t* conf)
 {
     printf("Hey.  I received a new config (userdata: %s):\n",
@@ -23,6 +33,9 @@ static void display_config(void* userdata, kvpair_t* conf)
     }
 }
 
+/*
+ * Example callback with a simple form result.
+ */
 static enum conflate_mgmt_cb_result process_stats(void *opaque,
                                                   conflate_handle_t *handle,
                                                   const char *cmd,
@@ -44,7 +57,9 @@ static enum conflate_mgmt_cb_result process_stats(void *opaque,
     return RV_OK;
 }
 
-
+/*
+ * Example callback that has an empty result.
+ */
 static enum conflate_mgmt_cb_result process_reset_stats(void *opaque,
                                                         conflate_handle_t *handle,
                                                         const char *cmd,
@@ -60,6 +75,9 @@ static enum conflate_mgmt_cb_result process_reset_stats(void *opaque,
     return RV_OK;
 }
 
+/*
+ * Example callback that returns a list of forms.
+ */
 static enum conflate_mgmt_cb_result process_ping_test(void *opaque,
                                                       conflate_handle_t *handle,
                                                       const char *cmd,
@@ -74,7 +92,10 @@ static enum conflate_mgmt_cb_result process_ping_test(void *opaque,
     char **servers = servers_p->values;
 
     for (int i = 0; servers[i]; i++) {
+        /* For each result we wish to send back, we begin a field set */
         conflate_next_fieldset(r);
+
+        /* All fields added now fall within the current fieldset */
         conflate_add_field(r, "-set-", servers[i]);
 
         const char *vals[3] = { "val1", "val2", NULL };
@@ -88,14 +109,16 @@ static enum conflate_mgmt_cb_result process_ping_test(void *opaque,
 
 int main(int argc, char **argv) {
 
-    conflate_config_t conf;
-
     if (argc < 3) {
         fprintf(stderr, "Usage: bot <jid> <pass> [host]\n");
         exit(EX_USAGE);
     }
 
-    /* Initialize callbacks for provided functionality */
+    /* Initialize callbacks for provided functionality.
+     *
+     * These callbacks are global and thus initialized once for your
+     * entire application.
+     */
     conflate_register_mgmt_cb("client_stats", "Retrieves stats from the agent",
                               process_stats);
     conflate_register_mgmt_cb("reset_stats", "Reset stats on the agent",
@@ -104,8 +127,11 @@ int main(int argc, char **argv) {
     conflate_register_mgmt_cb("ping_test", "Perform a ping test",
                               process_ping_test);
 
+    /* Perform default configuration initialization. */
+    conflate_config_t conf;
     init_conflate(&conf);
 
+    /* Specify application-specific configuration parameters. */
     conf.jid = argv[1];
     conf.pass = argv[2];
     conf.host = (argc == 4 ? argv[3] : NULL);
@@ -117,10 +143,14 @@ int main(int argc, char **argv) {
     conf.userdata = "something awesome";
     conf.new_config = display_config;
 
+    /* Begin the connection. */
     if(!start_conflate(conf)) {
         fprintf(stderr, "Couldn't initialize libconflate.\n");
         exit(1);
     }
 
+    /* Typically, your application would go about its business here.
+       Since this is just a demo, the caller has nothing else to do,
+       so we wait forever. */
     pause();
 }
