37132e38857a07be4fd4def8db114ed5f407675b
Dustin Sallings <dustin@spy.net>
2009-07-02 12:11:43 -0700

    Don't use the underlying strdup function.
    I've wrapped strdup in general, but it fails with ICC on Linux do to a
    strange definition of strdup anyway.

--- util.c
+++ util.c
@@ -6,10 +6,11 @@
 #include "conflate.h"
 
 char* safe_strdup(const char* in) {
-    char *rv = NULL;
     assert(in);
-    rv = strdup(in);
+    int len = strlen(in);
+    char *rv = calloc(len + 1, sizeof(char));
     assert(rv);
+    memcpy(rv, in, len);
     return rv;
 }
 
