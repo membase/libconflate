24741f36b2c15469948d13651679214f50cd598c
Matt Ingenthron <ingenthr@cep.net>
2009-09-30 17:51:07 -0700

    Added workaround for MacOS libtool and updated libstrophe.

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 mkdir -p m4
-libtoolize
+UNAME=`uname -s`
+if [ $UNAME = "Darwin" ]; then
+glibtoolize --automake
+else
+libtoolize --automake
+fi
 
 ACLOCAL=`which aclocal-1.10 || which aclocal-1.9 || which aclocal19 || which aclocal-1.7 || which aclocal17 || which aclocal-1.5 || which aclocal15 || which aclocal`
 # ACLOCALFLAGS may need to be defined if you have macros in non-standard
