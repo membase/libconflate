8b2c7fc1dead456022e2fe5a33710842415e6b61
CaptTofu <patg@patg.net>
2009-10-05 19:11:16 -0400

    Added search for automake/aclocal 1.11

--- autogen.sh
+++ autogen.sh
@@ -8,7 +8,7 @@ else
 libtoolize --automake
 fi
 
-ACLOCAL=`which aclocal-1.10 || which aclocal-1.9 || which aclocal19 || which aclocal-1.7 || which aclocal17 || which aclocal-1.5 || which aclocal15 || which aclocal`
+ACLOCAL=`which aclocal-1.11 || which aclocal-1.10 || which aclocal-1.9 || which aclocal19 || which aclocal-1.7 || which aclocal17 || which aclocal-1.5 || which aclocal15 || which aclocal`
 # ACLOCALFLAGS may need to be defined if you have macros in non-standard
 # locations, i.e. MacOS
 $ACLOCAL $ACLOCALFLAGS || exit 1
@@ -16,7 +16,7 @@ $ACLOCAL $ACLOCALFLAGS || exit 1
 AUTOHEADER=${AUTOHEADER:-autoheader}
 $AUTOHEADER || exit 1
 
-AUTOMAKE=`which automake-1.10 || which automake-1.9 || which automake-1.7 || exit 1`
+AUTOMAKE=`which automake-1.11 || which automake-1.10 || which automake-1.9 || which automake-1.7 || exit 1`
 $AUTOMAKE --foreign --add-missing || automake --gnu --add-missing || exit 1
 
 AUTOCONF=${AUTOCONF:-autoconf}
