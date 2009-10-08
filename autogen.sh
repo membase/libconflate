f9fdfd366ccd8e00c98c30ade30be2464bcc1c5c
CaptTofu <patg@patg.net>
2009-10-08 11:35:31 -0400

    Changes to autogen for build on OSX

--- autogen.sh
+++ autogen.sh
@@ -2,13 +2,14 @@
 
 mkdir -p m4
 UNAME=`uname -s`
+set -x
 if [ $UNAME = "Darwin" ]; then
 glibtoolize --automake
 else
 libtoolize --automake
 fi
 
-ACLOCAL=`which aclocal-1.11 || which aclocal-1.10 || which aclocal-1.9 || which aclocal19 || which aclocal-1.7 || which aclocal17 || which aclocal-1.5 || which aclocal15 || which aclocal`
+ACLOCAL=`which aclocal || which aclocal-1.11 || which aclocal-1.10 || which aclocal-1.9 || which aclocal19 || which aclocal-1.7 || which aclocal17 || which aclocal-1.5 || which aclocal15`
 # ACLOCALFLAGS may need to be defined if you have macros in non-standard
 # locations, i.e. MacOS
 $ACLOCAL $ACLOCALFLAGS || exit 1
