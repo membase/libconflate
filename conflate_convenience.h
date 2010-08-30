b35e6fd386032aab728a13786087ecd4f4e5d7ac
Trond Norbye <trond.norbye@gmail.com>
2010-08-30 08:53:56 +0200

    Fixed the varargs macro to be C99 compatible
    Change-Id: Ib777631e6beeac3f7b9d5fded81b9e116c22a31c
    Reviewed-on: http://review.northscale.com/2184
    Reviewed-by: Dustin Sallings <dustin@spy.net>
    Tested-by: Trond Norbye <trond.norbye@gmail.com>

--- conflate_convenience.h
+++ conflate_convenience.h
@@ -1,4 +1,8 @@
 /* Convenience stuff that shouldn't be part of the API */
+#ifndef CONFLATE_CONVENIENCE_H
+#define CONFLATE_CONVENIENCE_H 1
 
-#define CONFLATE_LOG(handle, level, msg, args...)                   \
-    handle->conf->log(handle->conf->userdata, level, msg, ##args)
+#define CONFLATE_LOG(handle, level, ...) \
+   handle->conf->log(handle->conf->userdata, level, __VA_ARGS__)
+
+#endif
